c22b7487726ab6c7afe21640d99b60646d9d7a7a
nadinn20 <nadja.shpytkovska@gmail.com>
2015-12-15 17:31:44 +0000

    changes

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function() {
+    $.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?q=istanbul'+
+    '&units=metric&cnt=7&APPID=e522dd5a533738e9c18cde03828c57b1',
+    function(data) {
+        data.list[0].temp.day;
+        $('#tempToday').html(data.list[0].temp.day);
+        $('#tempTomorrow').html(data.list[1].temp.day);
+        $('#tempAfterTommorow').html(data.list[2].temp.day);
+    }
+  );
+});
